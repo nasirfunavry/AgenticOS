2025a8452cadbdd38ed09c010447f7e4a26e75ee
Muhammad Ihtisham <m.ihtisham@funavry.com>
2025-04-30 16:08:06 +0500

    ejs setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { env } from "./config/env";
 import apiRouter from "./routes";
 import { scheduleTweets } from "./jobs/tweet.job";
-
+import ejs from "ejs";
+import { join } from "path";
 
 // Create Hono app
 const app = new Hono();
@@ -12,6 +14,7 @@ const app = new Hono();
 // Middleware
 app.use("*", logger());
 app.use("*", prettyJSON());
+app.use("/style.css", serveStatic({ root: "./public" }));
 
 // Default route
 app.get("/", (c) => {
@@ -20,6 +23,27 @@ app.get("/", (c) => {
     version: "1.0.0",
     status: "running",
   });
+});
+
+app.get("/test", async (c) => {
+  // First, render the dashboard content
+
+  // Fetch from file
+  const data = {};
+
+  const dashboardContent = await ejs.renderFile(join(import.meta.dir, "../views/dashboard.ejs"), {
+    title: "Dashboard",
+    array: ["item1", "item2", "item3"],
+    data: data,
+  });
+
+  // Then, inject it into the layout
+  const html = await ejs.renderFile(join(import.meta.dir, "../views/layout.ejs"), {
+    title: "Dashboard",
+    body: dashboardContent,
+  });
+
+  return c.html(html);
 });
 
 // API routes
